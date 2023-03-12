a4eff46566c995a80893fa162c90954669f08ffe
AlekseiShvaiko <aarkaklim@gmail.com>
2023-03-12 13:06:07 +0400

    Решена задача №2

---
+++ task2.py
@@ -0,0 +1,7 @@
+def print_operation_table(operation, num_rows=6, num_columns=6):
+    for row in range(1, num_rows+1):
+        for col in range(1, num_columns+1):
+            print(f"{operation(row, col):<3}", end="")
+        print()
+
+print_operation_table(lambda x, y: x * y)
